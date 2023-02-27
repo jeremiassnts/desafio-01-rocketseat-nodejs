2d2ff85429b3927be6a0a0108fed430ed2e1b349
Jeremias Santos <jeremias.santos@newcheck.com.br>
2023-02-27 09:32:28 -0300

    feat: get

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from 'node:http'
+import { routes } from './routes.js'
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
 
+    const route = routes.find(route => {
+        return route.method == method && route.path.test(url)
+    })
+    if (route) {
+        return route.handler(req, res)
+    }
+
+    return res.writeHead(404).end()
 })
 
 server.listen(3333, () => {
